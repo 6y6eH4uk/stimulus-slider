8064e7185b8b61ee9496d1dcd7041543a7541743
6y6eH4uk <35969123+6y6eH4uk@users.noreply.github.com>
2021-05-28 16:28:17 +0300

    Create stimulus-slider.js

---
+++ stimulus-slider.js
@@ -0,0 +1,38 @@
+class SliderController extends Stimulus.Controller {
+		static targets = ['slider']
+    
+    static values = {
+        slidesToShow: Number,
+        slidesToScroll: Number
+    }
+
+    initialize() {
+        $(this.sliderTarget).slick({
+            slidesToShow: this.slidesToShowValue,
+            slidesToScroll: this.slidesToScrollValue,
+            arrows: false
+        })
+    }
+
+    prev() {
+        $(this.sliderTarget).slick('slickPrev')
+    }
+
+    next() {
+        $(this.sliderTarget).slick('slickNext')
+    }
+    
+    goTo({ currentTarget }) {
+    		$(this.sliderTarget).slick('slickGoTo', currentTarget.dataset.slideIndex)
+    }
+}
+
+let application;
+
+if (window.stimulusApplication) {
+	application	= window.stimulusApplication
+} else {
+	application = Stimulus.Application.start()
+}
+
+application.register('slider', SliderController)
